0d0d4c04a41ae2d2d17d1bd42d8bb8f4450a0ea8
danielmartensson <daniel.martensson100@outlook.com>
2020-12-02 21:00:29 +0100

    Added a new java file that can paths

--- GeneratePaths.java
+++ GeneratePaths.java
@@ -1,14 +1,22 @@
-
+import java.io.*;
 
 class GeneratePaths {
     public static void main(String[] args) {
     	// Get arguments, which is only one argument actully!
     	double procent =  Double.parseDouble(args[0]);
+    	if(procent < 0 || procent > 1) {
+    		System.out.println("Sorry! You need to enter a double value between 0 and 1");
+    		return;
+    	}
     	File[] allPictures = scanAllClassFolders();
     }
     
     private static File[] scanAllClassFolders() {
     	// First get names of the folders 0, 1, 2, etc
-    	File[] folders = new File().
+    	File[] folders = new File("Classes").listFiles(File::isDirectory);
+    	for(File folder : folders) {
+    		System.out.println(folder.getAbsolutePath());
+    	}
+    	return null;
     }
 }
