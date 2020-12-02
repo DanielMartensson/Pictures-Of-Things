04c758e3e43f0273bee7ee5e344b77afd452cd7c
danielmartensson <daniel.martensson100@outlook.com>
2020-12-02 20:43:20 +0100

    Update

---
+++ Classes/GeneratePaths.java
@@ -0,0 +1,14 @@
+
+
+class GeneratePaths {
+    public static void main(String[] args) {
+    	// Get arguments, which is only one argument actully!
+    	double procent =  Double.parseDouble(args[0]);
+    	File[] allPictures = scanAllClassFolders();
+    }
+    
+    private static File[] scanAllClassFolders() {
+    	// First get names of the folders 0, 1, 2, etc
+    	File[] folders = new File().
+    }
+}
